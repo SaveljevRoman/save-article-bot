f94504a526163af1d1b616f851168fb444a64607
roman <saveljev.roman@gmali.com>
2023-10-15 17:54:16 +0300

    third

---
+++ events/telegram/commands.go
@@ -0,0 +1,100 @@
+package telegram
+
+import (
+	"bot/lib/e"
+	"bot/lib/storage"
+	"errors"
+	"log"
+	"net/url"
+	"strings"
+)
+
+const (
+	RndCmd   = "/rnd"
+	HelpCmd  = "/help"
+	StartCmd = "/start"
+)
+
+func (tp *TgProcessor) doCmd(text string, chatID int, username string) error {
+	text = strings.TrimSpace(text)
+
+	log.Printf("got new command '%s' from '%s'", text, username)
+
+	if isAddCmd(text) {
+		return tp.savePage(chatID, text, username)
+	}
+
+	switch text {
+	case RndCmd:
+		return tp.sendRandom(chatID, username)
+	case HelpCmd:
+		return tp.sendHelp(chatID)
+	case StartCmd:
+		return tp.SendHello(chatID)
+	default:
+		return tp.tg.SendMessage(chatID, msgUnknownCommand)
+	}
+}
+
+func (tp *TgProcessor) savePage(chatID int, pageURL string, username string) (err error) {
+	defer func() { err = e.Wrap("can not do command: save page", err) }()
+
+	page := &storage.Page{
+		URL:      pageURL,
+		UserName: username,
+	}
+
+	isExist, err := tp.storage.IsExists(page)
+	if err != nil {
+		return err
+	}
+	if isExist {
+		return tp.tg.SendMessage(chatID, msgAlreadyExists)
+	}
+
+	if err := tp.storage.Save(page); err != nil {
+		return err
+	}
+
+	if err := tp.tg.SendMessage(chatID, msgSaved); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (tp *TgProcessor) sendRandom(chatID int, username string) (err error) {
+	defer func() { err = e.Wrap("can not command: can not send random", err) }()
+
+	page, err := tp.storage.PickRandom(username)
+	if err != nil && !errors.Is(err, storage.ErrNoSavedPages) {
+		return err
+	}
+	if errors.Is(err, storage.ErrNoSavedPages) {
+		return tp.tg.SendMessage(chatID, msgNoSavedPages)
+	}
+
+	if err := tp.tg.SendMessage(chatID, page.URL); err != nil {
+		return err
+	}
+
+	return tp.storage.Remove(page)
+}
+
+func (tp *TgProcessor) sendHelp(chatID int) error {
+	return tp.tg.SendMessage(chatID, msgHelp)
+}
+
+func (tp *TgProcessor) SendHello(chatID int) error {
+	return tp.tg.SendMessage(chatID, msgHello)
+}
+
+func isAddCmd(text string) bool {
+	return isUrl(text)
+}
+
+func isUrl(text string) bool {
+	u, err := url.Parse(text)
+
+	return err == nil && u.Host != ""
+}
--- events/telegram/telegram.go
+++ events/telegram/telegram.go
@@ -1,4 +1,118 @@
 package telegram
 
+import (
+	"bot/clients/telegram"
+	"bot/events"
+	"bot/lib/e"
+	"bot/lib/storage"
+	"errors"
+)
+
 type TgProcessor struct {
+	tg      *telegram.Client
+	offset  int
+	storage storage.Storage
+}
+
+type Meta struct {
+	ChatId   int
+	UserName string
+}
+
+var (
+	ErrUnknownEventType = errors.New("unknown event type")
+	ErrUnknownMetaType  = errors.New("unknown meta type")
+)
+
+func NewTgProcessor(client *telegram.Client, storage storage.Storage) *TgProcessor {
+	return &TgProcessor{
+		tg:      client,
+		storage: storage,
+	}
+}
+
+func (tp *TgProcessor) Fetch(limit int) ([]events.Event, error) {
+	updates, err := tp.tg.Updates(tp.offset, limit)
+	if err != nil {
+		return nil, e.Wrap("can not get events", err)
+	}
+
+	if len(updates) == 0 {
+		return nil, nil
+	}
+
+	res := make([]events.Event, 0, len(updates))
+
+	for _, u := range updates {
+		res = append(res, event(u))
+	}
+
+	tp.offset = updates[len(updates)-1].ID + 1
+
+	return res, nil
+}
+
+func (tp *TgProcessor) Process(event events.Event) error {
+	switch event.Type {
+	case events.Message:
+		return tp.processMessage(event)
+	default:
+		return e.Wrap("can not process message", ErrUnknownEventType)
+	}
+}
+
+func (tp *TgProcessor) processMessage(event events.Event) (err error) {
+	defer func() { err = e.Wrap("can not process message", err) }()
+	meta, err := meta(event)
+	if err != nil {
+		return err
+	}
+
+	if err := tp.doCmd(event.Text, meta.ChatId, meta.UserName); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func event(upd telegram.Update) events.Event {
+	updType := fetchType(upd)
+	res := events.Event{
+		Type: updType,
+		Text: fetchText(upd),
+	}
+
+	if updType == events.Message {
+		res.Meta = Meta{
+			ChatId:   upd.Message.Chat.ID,
+			UserName: upd.Message.From.Username,
+		}
+	}
+
+	return res
+}
+
+func fetchText(upd telegram.Update) string {
+	if upd.Message == nil {
+		return ""
+	}
+
+	return upd.Message.Text
+}
+
+func fetchType(update telegram.Update) events.Type {
+	if update.Message == nil {
+		return events.Unknown
+	}
+
+	return events.Message
+}
+
+func meta(event events.Event) (Meta, error) {
+	res, ok := event.Meta.(Meta)
+	if !ok {
+		return Meta{}, e.Wrap("can not get meta", ErrUnknownMetaType)
+	}
+
+	return res, nil
 }
